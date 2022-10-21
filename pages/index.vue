353448709d35e3e96b0eacd0af763a657b7f4538
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-10-21 10:07:00 -0300

    Perfil chumbado

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       color="primary"
   >
   </v-progress-circular> -->
-  <Home :tweets='tweets' />
+  <Home :tweets="tweets" />
 </template>
 
 <script>
@@ -36,11 +36,27 @@ export default {
         { divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          name: 'Nikola Tesla',
+          username: '@NikolaTesla',
+          data: '7 de outubro',
+          text: 'Inventor',
+          subtitle: 'Nikola Tesla foi um inventor, engenheiro eletrotécnico'
+        },
+        {
+          avatar: 'https://www3.unicentro.br/petfisica/wp-content/uploads/sites/54/2016/08/Tesla.jpg',
           name: 'Brunch this weekend?',
           username: '@qualquer',
           data: '4 de outubro',
           text: 'blablabla',
           subtitle: 'sdcfsdvfsddgfsd'
+        },
+        {
+          avatar: 'https://www.nobelprize.org/images/einstein-12923-portrait-medium.jpg',
+          name: 'Albert Einstein',
+          username: '@Albert Einstein',
+          data: '14 de março de 1879',
+          text: ' físico teórico alemão',
+          subtitle: 'Albert Einstein foi um físico teórico alemão que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica'
         }
       ]
     }
